9766c5d7cfd94f8d99e184a2b33917682a58b7a5
Iman Haris Hadi <korehadonut@gmail.com>
2020-06-05 00:31:05 +0800

    Initial Commit

---
+++ EffaAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EffaAR
+//
+//  Created by Haris Hadi on 05/06/2020.
+//  Copyright © 2020 Haris Hadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
